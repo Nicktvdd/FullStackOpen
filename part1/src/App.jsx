e54ba42489056bc26abca069369001b39332423d
Nick van den Dungen <nvan-den@student.hive.fi>
2023-10-05 20:06:11 +0300

    1.3 refactor into objects

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -28,20 +28,25 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14}
 
   return (
     <div>
       <Header name={course} />
-      <Content name={part1} exercises={exercises1} />
-      <Content name={part2} exercises={exercises2} />
-      <Content name={part3} exercises={exercises3} />
-      <Total one={exercises1} two={exercises2} three={exercises3}/>
+      <Content name={part1.name} exercises={part1.exercises} />
+      <Content name={part2.name} exercises={part2.exercises} />
+      <Content name={part3.name} exercises={part3.exercises} />
+      <Total one={part1.exercises} two={part2.exercises} three={part3.exercises}/>
     </div>
   )
 }
