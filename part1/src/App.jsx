3eb93e957d9e4a2f8eaf5f21d0118a7e448b6a6b
Nick van den Dungen <nvan-den@student.hive.fi>
2023-10-07 08:37:29 +0300

    1.4 map for aray, reduce for accumulation

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,57 +1,59 @@
+import React from 'react';
+
 const Header = (props) => {
-  console.log(props)
-  return(
+  return (
     <div>
-      <h1>{props.name}</h1>
+      <h1>{props.course}</h1>
     </div>
-  )
-}
+  );
+};
 
 const Content = (props) => {
-  console.log(props)
-  return(
+  return (
     <div>
-      <p>{props.name} {props.exercises}</p>
+        {props.parts.map((part, index) => (
+          <p key={index}>
+            {part.name} {part.exercises}</p>
+        ))}
     </div>
-  )
-}
+  );
+};
 
 const Total = (props) => {
-  console.log(props)
-  const exerciseTotal = props.one + props.two+props.three;
-  return(
-    <div>
-      <p>Number of exercises {exerciseTotal}</p>
-    </div>
-  )
-}
-
-const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const exercises = props.parts.map((part) => part.exercises);
+  const totalExercises = exercises.reduce((acc, curr) => acc + curr, 0);
 
   return (
     <div>
-      <Header name={course} />
-      <Content name={parts[0].name} exercises={parts[0].exercises} />
-      <Content name={parts[1].name} exercises={parts[1].exercises} />
-      <Content name={parts[2].name} exercises={parts[2].exercises} />
-      <Total one={parts[0].exercises} two={parts[1].exercises} three={parts[2].exercises}/>
+      <p>Number of exercises {totalExercises}</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+const App = () => {
+  const course = 'Half Stack application development';
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7,
+    },
+    {
+      name: 'State of a component',
+      exercises: 14,
+    },
+  ];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  );
+};
+
+export default App;
