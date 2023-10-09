f267957c9c2890c36a0ce6a37d59b5ce4e382aa0
Nick van den Dungen <nvan-den@student.hive.fi>
2023-10-09 12:15:45 +0300

    feedback buttons and saved variables

--- part1/1.6-1.14/src/App.jsx
+++ part1/1.6-1.14/src/App.jsx
@@ -1,14 +1,42 @@
 import { useState } from 'react'
 
+const Display = props => <div>{props.text} {props.value}</div>
+
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const setToGood = newValue => {
+    console.log('Good now', newValue)
+    setGood(newValue)
+  }
+  const setToNeutral = newValue => {
+    console.log('Neutral now', newValue)
+    setNeutral(newValue)
+  }
+  const setToBad = newValue => {
+    console.log('Bad now', newValue)
+    setBad(newValue)
+  }
   return (
     <div>
-      code here
+      <h1> give feedback </h1>
+      <Button handleClick={() => setToGood(good + 1)} text="good" />
+      <Button handleClick={() => setToNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setToBad(bad + 1)} text="bad" />
+      <h1>statistics</h1>
+      <Display text="good" value={good} />
+      <Display text="neutral" value={neutral} />
+      <Display text="bad" value={bad} />
     </div>
   )
 }
