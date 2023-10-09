423b5fac9e38dd8103a11f5e49e536f415da3a73
Nick van den Dungen <nvan-den@student.hive.fi>
2023-10-09 12:34:28 +0300

    1.7 done

--- part1/1.6-1.14/src/App.jsx
+++ part1/1.6-1.14/src/App.jsx
@@ -15,32 +15,19 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const setToGood = newValue => {
-    console.log('Good now', newValue)
-    setGood(newValue)
-  }
-  const setToNeutral = newValue => {
-    console.log('Neutral now', newValue)
-    setNeutral(newValue)
-  }
-  const setToBad = newValue => {
-    console.log('Bad now', newValue)
-    setBad(newValue)
-  }
   return (
     <div>
       <h1> give feedback </h1>
-      <Button handleClick={() => setToGood(good + 1)} text="good" />
-      <Button handleClick={() => setToNeutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => setToBad(bad + 1)} text="bad" />
-      <h1>statistics</h1>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <h1> statistics </h1>
       <Display text="good" value={good} />
       <Display text="neutral" value={neutral} />
       <Display text="bad" value={bad} />
       <Display text="all" value={bad+good+neutral} />
       <Display text="average" value={(good-bad) / (bad+good+neutral)} />
       <Display text="positive" value={(good / (bad+good+neutral)) * 100} percent='%'/>
-
     </div>
   )
 }
