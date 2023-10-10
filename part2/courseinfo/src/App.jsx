83625ed169fbbb954353c87b3f36fbd04a740aa3
Nick van den Dungen <nvan-den@student.hive.fi>
2023-10-10 12:21:09 +0300

    2.1 define component Course

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,50 +1,58 @@
 import React from 'react'
-const Header = ({ course }) => <h1>{course}</h1>
 
+const Header = ({ course }) => <h1>{course}</h1>
 const Total = ({ sum }) => <p>Number of exercises {sum}</p>
 
-const Part = ({ part }) => 
+const Part = ({ part }) =>
   <p>
     {part.name} {part.exercises}
   </p>
 
-const Content = ({ parts }) => 
+const Content = ({ parts }) =>
   <>
     <Part
-      part={parts[0]} 
+      part={parts[0]}
     />
     <Part
-      part={parts[1]} 
+      part={parts[1]}
     />
     <Part
-      part={parts[2]} 
-    />      
+      part={parts[2]}
+    />
   </>
 
-const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-
+const Course = ({ course }) => { 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total sum={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
     </div>
   )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+  return <Course course={course} />
 }
 
 export default App
