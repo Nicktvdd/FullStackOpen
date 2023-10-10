393cb5e16429cc5f787dac771871e49566e97b3d
Nick van den Dungen <nvan-den@student.hive.fi>
2023-10-10 13:59:50 +0300

    2.5 seperate Course module

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,44 +1,6 @@
 import React from 'react'
+import Course from './components/course'
 
-const Header = ({ course }) => {
-  return (
-    <h1>{course}</h1>)
-}
-
-const Total = ({ course }) => {
-  const exercises = course.parts.map((part) => part.exercises);
-  const totalExercises = exercises.reduce((acc, curr) => acc + curr, 0);
-
-  return (
-    <p><b>Number of exercises {totalExercises}</b></p>)
-}
-
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>)
-}
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map((part, index) => (
-        <Part key={index} part={parts[index]} />
-      ))}
-    </div>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total course={course} />
-    </div>
-  )
-}
 
 const App = () => {
   const courses = [
