e501ce5e22c6124b216abe08fe3a2bcb8c88a395
Nick van den Dungen <nvan-den@student.hive.fi>
2023-10-10 12:37:20 +0300

    before changing 'Part'

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 
-const Header = ({ course }) => <h1>{course}</h1>
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+const Header = ({ course }) => {
+  return (
+    <h1>{course}</h1>)
+}
 
-const Part = ({ part }) =>
-  <p>
-    {part.name} {part.exercises}
-  </p>
+const Total = ({ course }) => {
+  const exercises = course.parts.map((part) => part.exercises);
+  const totalExercises = exercises.reduce((acc, curr) => acc + curr, 0);
 
-const Content = ({ parts }) =>
-  <>
+  return (
+    <p>Number of exercises {totalExercises}</p>)
+}
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>)
+}
+
+const Content = ({ parts }) => {
+  return (<>
     <Part
       part={parts[0]}
     />
@@ -19,13 +31,15 @@ const Content = ({ parts }) =>
     <Part
       part={parts[2]}
     />
-  </>
+  </>)
+}
 
-const Course = ({ course }) => { 
+const Course = ({ course }) => {
   return (
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
+      <Total course={course} />
     </div>
   )
 }
@@ -49,6 +63,11 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       }
     ]
   }
