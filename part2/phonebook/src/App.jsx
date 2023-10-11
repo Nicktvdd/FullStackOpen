2b7bd1a59e872a0f5558370ed2ece7b627a17794
Nick van den Dungen <nvan-den@student.hive.fi>
2023-10-11 11:26:53 +0300

    2.6 Phonebook names being added

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,25 +1,49 @@
 import { useState } from 'react'
 import React from 'react'
 
+const Name = ({ name }) => {
+  return (
+   <li>{name}</li>
+  )
+ }
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
-  ]) 
+  ])
   const [newName, setNewName] = useState('')
 
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName,
+    }
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
+      <form onSubmit={addName}>
+        name: <input value={newName}
+          onChange={handleNameChange}
+          />
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>debug: {newName}</div>
+      <ul>
+        {persons.map(person =>
+          <Name key={person.name} name={person.name} />
+        )}
+      </ul>
     </div>
   )
 }
