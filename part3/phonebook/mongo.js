30e1e96c2a50c0bd1d1c57453f7a62658d2695ab
Nick van den Dungen <nvan-den@student.hive.fi>
2023-10-12 20:29:28 +0300

    3.12 mongoDB working

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url =
+  `mongodb+srv://nicktvdd:${password}@cluster0.crtbvnu.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: process.argv[3],
+  number: process.argv[4]
+})
+
+/* Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person)
+    })
+    mongoose.connection.close()
+  }) */
+
+person.save().then(result => {
+  console.log('person saved!')
+  mongoose.connection.close()
+})
