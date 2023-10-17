16439164f0dcf0d81c589b446de42eb019b83362
Nick van den Dungen <nvan-den@student.hive.fi>
2023-10-17 19:42:25 +0300

    regex phone number validation

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -20,7 +20,16 @@ const personSchema = new mongoose.Schema({
         minLength: 3,
         required: true
     },
-    number: String,
+    number: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function(number) {
+                return /^\d{2,3}-\d+$/.test(number);
+            },
+            message: "Phone number must be in the format XX-XXXXXXXX (e.g., 09-1234556) or XXX-XXXXXXXX (e.g., 040-22334455)"
+        }
+    }
 })
 
 personSchema.set('toJSON', {
