379c7d03fbca9eab56f630be9fec8a67cc838382
Nick van den Dungen <nvan-den@student.hive.fi>
2023-10-17 16:28:37 +0300

    correct use order and structure

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -5,11 +5,20 @@ const Person = require('./models/person')
 const morgan = require('morgan')
 const cors = require('cors')
 
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+  }
+
 //---------------------------------use---------------------------------------------
 
-app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
+app.use(requestLogger)
+app.use(express.static('dist'))
 app.use(morgan((tokens, req, res) => {
     return [
         tokens.method(req, res),
@@ -43,20 +52,17 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).
-    then(person => {
-        if (person) {
-            response.json(person)
-        }
-        else {
-        response.status(404).end()
-        }
-    })
-    .catch(error => {
-        console.log(error)
-        response.status(400).send({ error: 'malformatted id' })
-    })
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+        .then(note => {
+            if (note) {
+                response.json(note)
+            }
+            else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 //--------------------------------delete-------------------------------------------
@@ -92,6 +98,25 @@ app.post('/api/persons', (request, response) => {
         response.json(newPerson)
     })
 })
+
+//--------------------------------error-------------------------------------------
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+  }
+
+app.use(errorHandler)
 
 //------------------------------listen-----------------------------------------------
 const PORT = process.env.PORT
