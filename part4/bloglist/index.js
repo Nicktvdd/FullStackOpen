097464b7d2197afbe480a3d87ec2e39e7caf69a5
Nick van den Dungen <nvan-den@student.hive.fi>
2023-11-16 13:55:43 +0200

    refactored app

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,30 +1,4 @@
-require('dotenv').config();
-
-const express = require('express')
-const app = express()
-const Blog = require('./models/blog')
-const cors = require('cors')
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+const app = require('./app')
 
 const PORT = 3003
 app.listen(PORT, () => {
