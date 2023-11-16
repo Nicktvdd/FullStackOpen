406be8a33fb96c651b9bc876ef72f7744ab5da5c
Nick van den Dungen <nvan-den@student.hive.fi>
2023-11-16 13:36:19 +0200

    4.1 blog list working with POST

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,10 +1,49 @@
-const http = require('http')
+require('dotenv').config();
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const Blog = mongoose.model('Blog', blogSchema)
+
+/* if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+} */
+
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
