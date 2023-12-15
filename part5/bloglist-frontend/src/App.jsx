df0010a580f451b7d6ca48d06a8f409c903ab3c9
Nick van den Dungen <nvan-den@student.hive.fi>
2023-12-15 11:11:51 +0000

    adding blog

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -9,6 +9,9 @@ const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [newTitle, setNewTitle] = useState('')
+  const [newAuthor, setNewAuthor] = useState('')
+  const [newUrl, setNewUrl] = useState('')
 
   const [user, setUser] = useState(null)
 
@@ -34,75 +37,115 @@ const App = () => {
     }
   }
 
-  const handleLogout = async (event) => {
-    event.preventDefault()
+  const handleTitleChange = (event) => {
+    setNewTitle(event.target.value)
+  }
 
-    try {
-      window.localStorage.removeItem(
-        'loggedBlogappUser', JSON.stringify(user),
-        window.location.reload(false)
-      )
-      } catch (exception) {
-      setErrorMessage('You are already logged out')
+  const handleAuthorChange = (event) => {
+    setNewAuthor(event.target.value)
+  }
+
+  const handleUrlChange = (event) => {
+    setNewUrl(event.target.value)
+  }
+
+  const addBlog = async (event) => {
+    event.preventDefault()
+    const blogObject = {
+      title: newTitle,
+      author: newAuthor,
+      url: newUrl,
+    }
+    await blogService.create(blogObject)
+    returnedBlog => {
+      setBlogs(blogs.concat(returnedBlog))
+      setNewTitle('')
+      setNewAuthor('')
+      setNewUrl('')
+      setErrorMessage(`Added ${blogObject.title}`)
       setTimeout(() => {
         setErrorMessage(null)
       }, 5000)
+      // reload here? window.location.reload(false)
     }
   }
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs(blogs)
+
+const handleLogout = async (event) => {
+  event.preventDefault()
+
+  try {
+    window.localStorage.removeItem(
+      'loggedBlogappUser', JSON.stringify(user),
+      window.location.reload(false)
     )
-  }, [])
+  } catch (exception) {
+    setErrorMessage('You are already logged out')
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 5000)
+  }
+}
 
-  useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
-    if (loggedUserJSON) {
-      const user = JSON.parse(loggedUserJSON)
-      setUser(user)
-      blogService.setToken(user.token)
-    }
-  }, [])
-
-  const loginForm = () => (
-    <div>
-      <h2>Log in to application</h2>
-      <form onSubmit={handleLogin}>
-        <div>
-          username
-          <input
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-        <div>
-          password
-          <input
-            type="password"
-            value={password}
-            name="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button type="submit">login</button>
-      </form>
-    </div>
+useEffect(() => {
+  blogService.getAll().then(blogs =>
+    setBlogs(blogs)
   )
+}, [])
 
-  const blogForm = () => (
-    <div>
-      <h2>blogs</h2>
-      <p>
-        <span className="active-user">{user.name}</span> logged in{" "}
-        <button id="logout-btn" onClick={handleLogout}>
-          logout
-        </button>
-      </p>
+useEffect(() => {
+  const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+  if (loggedUserJSON) {
+    const user = JSON.parse(loggedUserJSON)
+    setUser(user)
+    blogService.setToken(user.token)
+  }
+}, [])
 
-{/*       <form onSubmit={addBlog}>
+const loginForm = () => (
+  <div>
+    <h2>Log in to application</h2>
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  </div>
+)
+
+const blogForm = () => (
+  <div>
+    <h2>blogs</h2>
+    <p>
+      <span className="active-user">{user.name}</span> logged in{" "}
+      <button id="logout-btn" onClick={handleLogout}>
+        logout
+      </button>
+    </p>
+    <form onSubmit={addBlog}>
+      <div>Title: <input value={newTitle} onChange={handleTitleChange} /></div>
+      <div>Author: <input value={newAuthor} onChange={handleAuthorChange} /></div>
+      <div>Url: <input value={newUrl} onChange={handleUrlChange} /></div>
+      <div><button type="submit">add</button></div>
+    </form>
+
+    {/*       <form onSubmit={addBlog}>
         <input
           value={newBlog}
           onChange={handleBlogChange}
@@ -110,26 +153,26 @@ const App = () => {
         <button type="submit">save</button>
       </form> */}
 
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+    {blogs.map(blog =>
+      <Blog key={blog.id} blog={blog} />
+    )}
 
 
 
-    </div>
-  )
+  </div>
+)
 
-  return (
-    <div>
-      <Notification message={errorMessage} />
+return (
+  <div>
+    <Notification message={errorMessage} />
 
-      {user === null ?
-        loginForm() :
-        blogForm()
-      }
+    {user === null ?
+      loginForm() :
+      blogForm()
+    }
 
-    </div>
-  )
+  </div>
+)
 }
 
 export default App
