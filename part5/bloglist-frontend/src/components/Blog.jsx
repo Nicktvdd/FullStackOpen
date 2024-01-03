68f93d63904d166f8f2ef96d6c4ca00ef8e8e15a
Nick van den Dungen <nicktvdd@hotmail.com>
2024-01-03 20:27:24 +0200

    almost finished 5.7 'added by user' is missing

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -3,20 +3,41 @@ import { useState } from 'react'
 const Blog = ({ blog }) => {
   const [showDetails, setShowDetails] = useState(false)
 
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
   const toggleDetails = () => {
     setShowDetails(!showDetails)
   }
+
+  const ExtraInfo = () => {
+    return(
+      <div>
+      <div>{blog.url}</div>
+      <div>likes: {blog.likes} <button>like</button></div>
+      <div>{blog.user}</div>
+      </div>
+      )
+   }
+
   return (
-    <blog>
+    <div style={blogStyle}>
       <div>{blog.title}
       <button onClick={toggleDetails}>{showDetails ? 'Hide Details' : 'Show Details'}</button>
       </div>
       <div>{blog.author}</div>
-      <div>{showDetails && blog.url}</div>
-    </blog>
-    
+      <div>{showDetails && <ExtraInfo />}</div>
+    </div>
   )
 }
+
+
+
 
 
 export default Blog
