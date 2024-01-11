f5253d065a74e0072fcca344eb2ae5ee3619cdca
nicktvdd <nicktvdd@gmail.com>
2024-01-11 15:52:48 +0200

    test component

---
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import Blog from './Blog'
+
+test('renders content', () => {
+  const blog = {
+    title: 'Component testing is done with react-testing-library',
+    author: 'me',
+    url: 'www.sheep.ue'
+
+  }
+
+  render(<Blog blog={blog} />)
+
+  const element = screen.getByText('Component testing is done with react-testing-library')
+  expect(element).toBeDefined()
+})
