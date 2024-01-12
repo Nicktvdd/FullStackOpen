cb683e51ea30d559b3ca6d863ada39b6f9450b1e
nicktvdd <nicktvdd@gmail.com>
2024-01-12 16:45:29 +0200

    5.13 testing

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders content', () => {
@@ -16,3 +17,44 @@ test('renders content', () => {
   const element = screen.getByText('Component testing is done with react-testing-library')
   expect(element).toBeDefined()
 })
+
+test('displays blogs tittle and author, but not url or number of likes', () => {
+  const blog = {
+    title: 'Component testing is done with react-testing-library',
+    author: 'me',
+    url: 'www.sheep.ue'
+  }
+
+  render(<Blog blog={blog} />)
+
+  const title = screen.getByText('Component testing is done with react-testing-library')
+  expect(title).toBeDefined()
+  const author = screen.getByText('me')
+  expect(author).toBeDefined()
+  const url = screen.queryByText('www.sheep.ue')
+  expect(url).toBeNull()
+  const likes = screen.queryByText('likes')
+  expect(likes).toBeNull()
+})
+
+
+/* test('clicking the button calls event handler once', async () => {
+  const blog = {
+    title: 'Component testing is done with react-testing-library',
+    author: 'me',
+    url: 'www.sheep.ue'
+  }
+
+  const mockHandler = jest.fn()
+
+  render(
+    <Blog blog={blog} toggleDetails={mockHandler} />
+  )
+
+  const user = userEvent.setup()
+  const button = screen.queryByText('Show Details')
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(1)
+})
+ */
